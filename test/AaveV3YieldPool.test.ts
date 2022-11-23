8cc0febbcd7695cb7432d1274bdc0c38e3200bf7
JakubKub13 <jakubkubala3@gmail.com>
2022-11-23 17:43:01 +0100

    adding test file boilerplate

---
+++ test/AaveV3YieldPool.test.ts
@@ -0,0 +1,6 @@
+import { Signer } from "ethers";
+import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
+import { expect } from 'chai';
+import { BigNumber } from 'ethers';
+import { MockContract } from 'ethereum-waffle';
+import { ethers, waffle } from 'hardhat';
