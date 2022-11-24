97a350ebadf36afb9e7a5a2ba081fc78c60f798a
JakubKub13 <jakubkubala3@gmail.com>
2022-11-24 10:20:37 +0100

    Adding state vars to test

--- test/AaveV3YieldPool.test.ts
+++ test/AaveV3YieldPool.test.ts
@@ -4,7 +4,7 @@ import { expect } from 'chai';
 import { BigNumber } from 'ethers';
 import { MockContract } from 'ethereum-waffle';
 import { ethers, waffle } from 'hardhat';
-import { AaveV3YieldSourceHarness, AaveV3YieldSourceHarness__factory, AavePool, ATokenMintable, ERC20Mintable,} from "../typechain-types"
+import { AaveV3YieldPool, AaveV3YieldPool__factory, AavePool, ATokenMintable, ERC20Mintable,} from "../typechain-types"
 import IRewardsController from "../artifacts/@aave/periphery-v3/contracts/rewards/interfaces/IRewardsController.sol/IRewardsController.json";
 import IPoolAddressesProvider from "../artifacts/@aave/core-v3/contracts/interfaces/IPoolAddressesProvider.sol/IPoolAddressesProvider.json";
 //import { IPoolAddressesProviderRegistry } from "../typechain-types";
@@ -18,5 +18,18 @@ const DECIMALS = 6;
 const toWei = (amount: string) => parseUnits(amount, DECIMALS);
 
 describe("AaveV3YieldPool", function () {
+    let contractsOwner: Signer;
+    let yieldPoolOwner: SignerWithAddress;
+    let wallet2: SignerWithAddress;
+    let attacker: SignerWithAddress;
+    let aToken: ATokenMintable;
+    let rewardsController: MockContract;
+    let pool: AavePool;
+    let poolAddressesProvider: MockContract;
+    let poolAddressesProviderRegistry: MockContract;
+    let aaveV3YieldPool: AaveV3YieldPool
+    let erc20Token: MockContract;
+    let usdcToken: ERC20Mintable;
     
+    let constructorTest = false;
 })
