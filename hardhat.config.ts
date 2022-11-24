7ce3949dceaeeaa878f27a4412d78abd30058158
JakubKub13 <jakubkubala3@gmail.com>
2022-11-24 13:26:30 +0100

    Refactoring hardhatconfig

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,19 +1,60 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import '@nomiclabs/hardhat-etherscan';
-import '@typechain/hardhat';
-import '@nomiclabs/hardhat-ethers';
-import '@nomiclabs/hardhat-waffle';
-import '@typechain/hardhat';
-import 'hardhat-abi-exporter';
-import 'hardhat-deploy';
-import 'hardhat-deploy-ethers';
-import 'hardhat-gas-reporter';
-import 'hardhat-log-remover';
-import 'solidity-coverage';
+import "dotenv/config"
+
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || ""
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "privatKey"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || ""
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || ""
+const OP_MAINNET_RPC_URL = process.env.OP_MAINNET_RPC_URL || ""
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.10",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      forking: {
+        url: OP_MAINNET_RPC_URL
+      },
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+    },
+    localhost: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+    },
+    goerli: {
+      url: GOERLI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 5,
+    },
+    mumbai: {
+      url: MUMBAI_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 80001,
+    },
+  },
+  solidity: {
+    compilers: [
+      {version: "0.8.10"},
+    ],
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+    //apiKey: POLYGONSCAN_API_KEY
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+  },
+  mocha: {
+    timeout: 200000, // 200 seconds max for running tests
+  },
 };
 
 export default config;
